661ca6e9bf16734a08786d497f89266ac863f1d7
sharkdp <davidpeter@web.de>
2019-06-20 00:31:33 +0200

    Add another example

--- test/example.cpp
+++ test/example.cpp
@@ -4,12 +4,15 @@ int main() {
   std::string message = "hello";
   dbg(message);  // [DEBUG example.cpp:5 (main)] message = "hello"
 
-  std::vector<int> numbers{1, 17, 42};
-  dbg(numbers);  // [DEBUG example.cpp:8 (main)] numbers = {1, 17, 42} (size=3)
+  const int a = 2;
+  const int b = dbg(3 * a) + 1;  // [DEBUG example.cpp:8 (main)] 3 * a = 6
 
-  dbg("this line is executed");  // [DEBUG example.cpp:10 (main)] this line is executed
+  std::vector<int> numbers{b, 13, 42};
+  dbg(numbers);  // [DEBUG example.cpp:11 (main)] numbers = {7, 13, 42} (size=3)
 
-  dbg(sizeof(int));  // [DEBUG example.cpp:12 (main)] sizeof(int) = 4
+  dbg("this line is executed");  // [DEBUG example.cpp:13 (main)] this line is executed
+
+  dbg(sizeof(int));  // [DEBUG example.cpp:15 (main)] sizeof(int) = 4
 
   return 0;
 }
