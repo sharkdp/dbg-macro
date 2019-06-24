e9b8aa61d179f7389f07b9371db3bb589985eb34
George Fotopoulos <xorz57@protonmail.com>
2019-06-24 13:38:37 +0300

    Better std::list Test

--- tests/src/test_list.cpp
+++ tests/src/test_list.cpp
@@ -2,7 +2,27 @@
 #include <list>
 
 int main(int argc, char const* argv[]) {
-  const std::list<int> list{-4, -3, -2, -1, 1, 2, 3, 4};
-  dbg(list);
+  std::list<int> list1{};
+  std::list<int> list2{1, 2, 3};
+
+  const std::list<int> list3{};
+  const std::list<int> list4{1, 2, 3};
+
+  dbg(list1);
+  dbg(list2);
+
+  dbg(list3);
+  dbg(list4);
+
+  dbg(std::list<int>{});
+  dbg(std::list<int>{1, 2, 3});
+
+  assert_eq(pretty_print(std::list<int>{}), "{}");
+  assert_eq(pretty_print(std::list<int>{1, 2, 3}), "{1, 2, 3}");
+
+  assert_eq(dbg_macro::type_name<std::list<int>>(), "std::list<int>");
+
+  assert_eq(dbg_macro::type_name<const std::list<int>>(), "const std::list<int>");
+
   return 0;
 }
