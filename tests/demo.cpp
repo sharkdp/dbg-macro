4f733129b4a2125a97f6593ab85015b58e8754bf
sharkdp <davidpeter@web.de>
2019-12-29 11:16:44 +0100

    Use catch2 for unit testing

---
+++ tests/demo.cpp
@@ -0,0 +1,113 @@
+#include <array>
+#include <cstdint>
+#include <iostream>
+#include <list>
+#include <memory>
+#include <sstream>
+#include <string>
+#include <tuple>
+#include <vector>
+
+#if __cplusplus >= 201703L
+#include <optional>
+#include <variant>
+#endif
+
+#include <dbg.h>
+
+int main() {
+  dbg("====== primitive types");
+
+  int test_int = 42;
+  const float test_float = 3.14;
+  const bool test_bool = false;
+  const char test_char = 'X';
+  const uint64_t test_uint64_t = 12345678987654321;
+  int* test_pointer = &test_int;
+  const int* test_pointer_to_const = &test_int;
+  int* test_pointer_null = nullptr;
+  const int& test_ref_to_int = test_int;
+  const char* test_c_string = "hello";
+  const char test_c_chararray[] = "hello";
+  const std::string test_string = "hello";
+
+  dbg(test_int);
+  dbg(test_float);
+  dbg(test_bool);
+  dbg(test_char);
+  dbg(test_uint64_t);
+  dbg(test_pointer);
+  dbg(test_pointer_to_const);
+  dbg(test_pointer_null);
+  dbg(test_ref_to_int);
+  dbg(test_c_string);
+  dbg(test_c_chararray);
+  dbg(test_string);
+
+  dbg("====== r-values, literals, constants, etc.");
+
+  dbg(42);
+  dbg(3.14);
+  dbg(false);
+  dbg(12345678987654321);
+  dbg(static_cast<void*>(nullptr));
+  dbg("string literal");
+
+  std::string message = "hello world";
+  dbg(std::move(message));
+
+  dbg(sizeof(int));
+
+  dbg("====== expressions inside macros");
+
+  dbg(9 + 33);
+  dbg(test_string + " world");
+
+  dbg("====== containers");
+
+  const std::vector<int> dummy_vec_int{3, 2, 3};
+  dbg(dummy_vec_int);
+
+  std::vector<int> dummy_vec_int_nonconst{1, 2, 3};
+  dbg(dummy_vec_int_nonconst);
+
+  const std::vector<int> dummy_vec_empty{};
+  dbg(dummy_vec_empty);
+
+  std::vector<char> vec_chars{'h', 'e', 'l', 'l', 'o', '\x00', '\xFE'};
+  dbg(vec_chars);
+
+  std::vector<bool> vec_bools{true, true, false, false, false, true, false};
+  dbg(vec_bools);
+
+  dbg(std::vector<int>{0, 1, 0, 1});
+
+  const std::array<int, 2> dummy_array{{0, 4}};
+  dbg(dummy_array);
+
+  const std::list<int> dummy_list{1, 2, 3, 4, 5, 6, 7, 8, 9};
+  dbg(dummy_list);
+
+  std::vector<std::vector<int>> vec_of_vec_of_ints{{1, 2}, {3, 4, 5}};
+  dbg(vec_of_vec_of_ints);
+
+  std::vector<std::vector<std::vector<int>>> vec_of_vec_of_vec_of_ints{
+      {{1, 2}, {3, 4, 5}}, {{3}}};
+  dbg(vec_of_vec_of_vec_of_ints);
+
+  int dummy_int_array[] = {11, 22, 33};
+  dbg(dummy_int_array);
+
+  dbg("====== function name tests");
+
+  class user_defined_class {
+   public:
+    void some_method(int x) { dbg(x); }
+  };
+  user_defined_class{}.some_method(42);
+
+  [](int x) {
+    dbg("called from within a lambda!");
+    return x;
+  }(42);
+}
