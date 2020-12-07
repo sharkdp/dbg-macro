26f0185d27eb8103c803d2e02d33db03567c5144
權少 <ljqican@163.com>
2020-12-07 20:42:25 +0800

    1. fixed std::string_view test. 2. add Zc:__cplusplus on Windows. 3. Use the system Catch2 when installed.

--- dbg.h
+++ dbg.h
@@ -635,6 +635,11 @@ inline bool pretty_print(std::ostream& stream,
   return true;
 }
 
+inline bool pretty_print(std::ostream& stream, const std::string_view& value) {
+  stream << '"' << value << '"';
+  return true;
+}
+
 #endif
 
 template <typename T, typename... U>
