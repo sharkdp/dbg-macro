a77c36e1c229d6c09745d5ce5e98586b5b3c5f8c
Ashwin A Nayar <ashwin5059198@gmail.com>
2023-01-15 16:00:16 +0530

    Refactor example and readme

--- tests/example.cpp
+++ tests/example.cpp
@@ -1,8 +1,9 @@
 #include <dbg.h>
+#include <cstdint>
 #include <vector>
 
 // You can use "dbg(..)" in expressions:
-int factorial(int n) {
+int32_t factorial(int32_t n) {
   if (dbg(n <= 1)) {
     return dbg(1);
   } else {
@@ -10,19 +11,17 @@ int factorial(int n) {
   }
 }
 
-int main() {
+int32_t main() {
   std::string message = "hello";
   dbg(message);  // [example.cpp:15 (main)] message = "hello" (std::string)
 
-  const int a = 2;
-  const int b = dbg(3 * a) + 1;  // [example.cpp:18 (main)] 3 * a = 6 (int)
+  const int32_t a = 2;
+  const int32_t b = dbg(3 * a) + 1;  // [example.cpp:18 (main)] 3 * a = 6 (int32_t)
 
-  std::vector<int> numbers{b, 13, 42};
-  dbg(numbers);  // [example.cpp:21 (main)] numbers = {7, 13, 42}
-                 // (std::vector<int>)
+  std::vector<int32_t> numbers{b, 13, 42};
+  dbg(numbers);  // [example.cpp:21 (main)] numbers = {7, 13, 42} (std::vector<int32_t>)
 
-  dbg("this line is executed");  // [example.cpp:23 (main)] this line is
-                                 // executed
+  dbg("this line is executed");  // [example.cpp:23 (main)] this line is executed
 
   factorial(4);
 
